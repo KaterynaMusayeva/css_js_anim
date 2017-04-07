c8936842a87d145eb0410bc9b0303e52c8238a35
Kateryna <kateryna.musayeva@railsware.com>
2017-04-07 17:33:02 +0300

    Object rotation task fixes.

--- source/javascripts/sm-rotate.js
+++ source/javascripts/sm-rotate.js
@@ -2,19 +2,11 @@
 		// init controller
 		var controller = new ScrollMagic.Controller();
 
-    // get the current scroll Position
-    // var scrollPos = controller.scrollPos();
-
-    // set a new scroll position calculation method
-    // controller.scrollPos(function () {
-        // return this.info("vertical") ? -circle.y : -circle.x
-        // });
-
 		// build tween
 		var tween = TweenMax.to("#target", 1, {rotation: 360, ease: Linear.easeNone});
 
 		// build scene
-		var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500})
+		var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 400})
 						.setTween(tween)
 						.addTo(controller);
 
